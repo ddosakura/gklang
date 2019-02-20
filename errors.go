acdd45b137e9e126c7aa3d3072fc716ecfcaf150
Sakura Mo <moyinzi@outlook.com>
2019-02-20 17:41:40 +0800

    add Err func to deal no logger loaded

--- errors.go
+++ errors.go
@@ -1,6 +1,15 @@
 package gklang
 
+import (
+	"log"
+)
+
 // Er exit with error log
 func Er(e error) {
 	Log(LCrash, e)
+}
+
+// Err used when no logger has be loaded
+func Err(e error) {
+	log.Fatalln(e)
 }
